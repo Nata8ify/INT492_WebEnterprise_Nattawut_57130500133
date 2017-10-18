a93b61a1b65dbb5af173a071f98e647338458111
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-10-18 23:16:18 +0700

    MAKE Somethings

--- src/main/java/com/arms/configuration/WebMvcConfiguration.java
+++ src/main/java/com/arms/configuration/WebMvcConfiguration.java
@@ -9,7 +9,6 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		// TODO Auto-generated method stub
 		registry.addRedirectViewController("/", "/user/list");
 		registry.addViewController("/login").setViewName("user/login");
 		
